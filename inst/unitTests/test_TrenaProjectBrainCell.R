f1cd308497dd5b8c851b8303152879aef5a259e4
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-08-21 13:16:50 -0700

    all tests pass

--- inst/unitTests/test_TrenaProjectBrainCell.R
+++ inst/unitTests/test_TrenaProjectBrainCell.R
@@ -18,6 +18,7 @@ runTests <- function()
    test_setTargetGene()
    test_buildSingleGeneModel()
    test_buildSingleGeneModel_slowGenes()
+   test_buildSingleGeneModel_footprintsAndWithout_MEF2C()
 
 } # runTests
 #------------------------------------------------------------------------------------------------------------------------
@@ -67,11 +68,17 @@ test_expressionMatrices <- function()
    checkTrue(all(expected.matrix %in% getExpressionMatrixNames(tp)))
 
    mtx <- getExpressionMatrix(tp, expected.matrix)
-   checkEquals(dim(mtx), c(50281,  264))
-   expected.genes <- c("A1BG", "A1CF", "A2M")
+   checkEquals(dim(mtx), c(13974, 264))
+   expected.genes <- c("AACS", "AADAT", "AAED1")
    checkTrue(all(expected.genes %in% rownames(mtx)))
 
-   checkTrue(max(mtx) < 15)
+   summary.stats <- fivenum(mtx)
+
+   checkTrue(summary.stats[1] < 0.01)
+   checkTrue(summary.stats[1] > 0)
+
+   checkTrue(summary.stats[5] < 10)
+   checkTrue(summary.stats[5] > 9)
 
 } # test_expressionMatrices
 #------------------------------------------------------------------------------------------------------------------------
@@ -103,14 +110,13 @@ test_setTargetGene <- function()
    checkEquals(region$chromLocString, "chr6:31399784-31415315")
 
    message(sprintf("    enhancers"))
-   tbl.enhancers <- getEnhancers(tp)
-   checkEquals(colnames(tbl.enhancers), c("chrom", "start", "end", "type", "combinedScore", "geneSymbol"))
+   tbl.enhancers <- getEnhancers(tp, tissues=c("brain", "Brain"))
+   checkEquals(head(colnames(tbl.enhancers)), c("chrom", "start", "end",  "gene", "eqtl",  "hic"))
    checkTrue(nrow(tbl.enhancers) >= 0)
 
    message(sprintf("    geneGeneEnhancersRegion"))
    region <- getGeneEnhancersRegion(tp, flankingPercent=0)
    checkTrue(all(c("chromLocString", "chrom", "start", "end") %in% names(region)))
-   checkEquals(region$chromLocString, "chr6:30554823-32372101")
 
    message(sprintf("    encode DHS"))
    tbl.dhs <- getEncodeDHS(tp)
@@ -203,16 +209,17 @@ test_buildSingleGeneModel_slowGenes <- function()
 
    target.gene.among.tfs <- match(targetGene, other.tfs)
    if(!is.na(target.gene.among.tfs))
-   other.tfs <- other.tfs[-target.gene.among.tfs]
+      other.tfs <- other.tfs[-target.gene.among.tfs]
 
    mtx.test <- mtx[other.tfs,]
+   dim(mtx.test)
 
    correlations <- abs(apply(mtx.test, 1, function(row) cor(target.gene.expression, row)))
    range.of.correlations <- fivenum(correlations)
    third.quartile <- range.of.correlations[4]
    max <- range.of.correlations[5]
 
-   if(third.quartile > 0.25) # then proceed
+   #if(third.quartile < 0.25) return()
 
    build.spec <- list(title=sprintf("unit test on %s", targetGene),
                       type="footprint.database",
@@ -289,9 +296,9 @@ test_buildSingleGeneModel_RBMXP2 <- function()
 } # test_buildSingleGeneModel_RBMXP2
 #------------------------------------------------------------------------------------------------------------------------
 # build mef2c model with footprints, and simply based on expression (with all TFs)
-test_buildSingleGeneModel_MEF2C <- function()
+test_buildSingleGeneModel_footprintsAndWithout_MEF2C <- function()
 {
-   printf("--- test_buildSingleGeneModel_MEF2C")
+   printf("--- test_buildSingleGeneModel_footprintsAndWithout_MEF2C")
 
    genome <- "hg38"
    targetGene <- "MEF2C"
@@ -346,20 +353,21 @@ test_buildSingleGeneModel_MEF2C <- function()
                         annotationDbFile=dbfile(org.Hs.eg.db),
                         orderModelByColumn="rfScore",
                         solverNames=c("lasso", "lassopv", "pearson", "randomForest", "ridge", "spearman"),
-                        quiet=TRUE)
+                        quiet=FALSE)
 
-   builder <- NoDnaModelBuilder(genome, targetGene,  recipe.noDNA, quiet=FALSE
-   x <- build(builder)
-   checkEquals(x$regulatoryRegions, data.frame())
-   tbl.model <- x$model
-      # with a relaxed tfPrefilterCorrelation, and the hand-picked TFs listed above
-      # all but "bogus" make the cut
+   builder <- NoDnaModelBuilder(genome, targetGene, recipe.noDNA, quiet=FALSE)
+   x.noDNA <- build(builder)
 
+   checkEquals(x.noDNA$regulatoryRegions, data.frame())
+   tbl.model <- x.noDNA$model
 
 
-} # test_buildSingleGeneModel_RBMXP2
+   tfs.in.both <- intersect(head(tbl.model)$gene, head(x.fp$model)$gene)
+   checkTrue(all(c("PKNOX2", "HLF", "DLX1") %in% tfs.in.both))
+   new.tfs.withoutBindingSites <- setdiff(head(tbl.model)$gene, head(x.fp$model)$gene)
+   checkTrue("CSRNP3" %in% new.tfs.withoutBindingSites)
+
+} #  test_buildSingleGeneModel_footprintsAndWithout_MEF2C
 #------------------------------------------------------------------------------------------------------------------------
-
-
-
-
+if(!interactive())
+   runTests()
